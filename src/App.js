01cdff07123a9d1e2ba50089218de6fe2f39d3dd
AndrewPru <58787232+AndrewPru@users.noreply.github.com>
2020-08-30 20:57:31 +0300

    Created components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-    Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <h1>Привет, Женя</h1>
-    <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-    Learn React
-    </a>
-    </header>
-    </div>
-    );
-  }
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+        )
+    }
+}
 
   export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <h1>Самый лучший сайт про кофе</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
